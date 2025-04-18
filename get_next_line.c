2c2ed5ff58cb2e974c000d1a2093e7b341e48188
TiagoDev88 <>
2025-04-18 16:04:52 +0100

    new teste

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
@@ -6,17 +6,80 @@
 /*   By: tfilipe- <tfilipe-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:38:06 by tfilipe-          #+#    #+#             */
-/*   Updated: 2025/04/17 23:08:45 by tfilipe-         ###   ########.fr       */
+/*   Updated: 2025/04/18 16:03:48 by tfilipe-         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// ft_strjoin: Junto duas strings (ex: stash + buffer)
+
+// ft_strchr: vai procurar \n na stash
+
+// ft_strdup: Copia uma string
+
+// ft_substr: Extrai parte da stash
+
+// ft_strlen: Retorna tamanho da string
+
+// Alguma função para limpar e gerir a memória
+// (free, malloc, etc.)
+// (não esquecer de libertar a stash no final)
+
+
 char	*get_next_line(int fd)
 {
-	
+	static char	*buffer;
+	char 		buffer_to_read[BUFFER_SIZE + 1];
+	ssize_t 	buffersize;
+	ssize_t 	read_bytes;
+
+	buffer = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffersize = BUFFER_SIZE;
+	read_bytes = 1;
+	//     declarar buffer temporário com tamanho BUFFER_SIZE
+	//     inicializar variável de controlo de leitura (bytes_lidos)
+	while (*buffer_to_read != '\n' && read_bytes != 0)
+	{
+		read_bytes = read(fd, buffer_to_read, buffersize);
+		// if (read_bytes < 0)
+		// {
+		// 	free(read_bytes);
+		// 	return (NULL);
+		// }
+		buffer_to_read[read_bytes] = '\0';
+		write(1, &buffer_to_read, read_bytes);
+	}
+
+//     enquanto '\n' não estiver no stash E bytes_lidos != 0
+//         bytes_lidos = ler do fd para o buffer
+//         se bytes_lidos < 0
+//             libertar memória, retornar NULL
+//         concatenar buffer ao stash
+	if (buffer == 0)
+		return (NULL);
+//     se stash está vazio
+//         retornar NULL
+
+//     encontrar índice do '\n' (ou fim da string se não existir)
+//     copiar do início até '\n' para uma nova string chamada "linha"
+    
+//     atualizar stash com o que sobrou depois do '\n'
+    
+//     retornar "linha"
 }
 
+#include <fcntl.h>
+int main()
+{
+	int fd = open("test.txt", O_RDONLY);
+	get_next_line(fd);
+	close(fd);
+	fd = open("test.txt", O_RDONLY);
+	get_next_line(fd);
+}
 // get_next_line: Lê uma linha de um arquivo (fd) e retorna-a como string
 // (incluindo o '\n' se existir).
 // A função lê o arquivo até encontrar um '\n' ou o fim do arquivo (EOF).
@@ -40,45 +103,9 @@ char	*get_next_line(int fd)
 // garantir que a memória alocada para a string seja liberada corretamente após o uso.
 
 
-
 // para evitar desperdício de memória. A função deve ser eficiente e não deve
 //causar vazamentos de memória.
 
-// função get_next_line(fd)
-//     declarar static variável chamada stash por exemplo ou outro nome ( para guardar dados entre chamadas)
-
-//     se fd inválido ou BUFFER_SIZE <= 0
-//         retornar NULL
-
-//     declarar buffer temporário com tamanho BUFFER_SIZE
-//     inicializar variável de controlo de leitura (bytes_lidos)
-
-//     enquanto '\n' não estiver no stash E bytes_lidos != 0
-//         bytes_lidos = ler do fd para o buffer
-//         se bytes_lidos < 0
-//             libertar memória, retornar NULL
-//         concatenar buffer ao stash
-
-//     se stash está vazio
-//         retornar NULL
-
-//     encontrar índice do '\n' (ou fim da string se não existir)
-//     copiar do início até '\n' para uma nova string chamada "linha"
-    
-//     atualizar stash com o que sobrou depois do '\n'
-    
-//     retornar "linha"
-
-// ft_strjoin: Junto duas strings (ex: stash + buffer)
-
-// ft_strchr: vai procurar \n na stash
-
-// ft_strdup: Copia uma string
-
-// ft_substr: Extrai parte da stash
-
-// ft_strlen: Retorna tamanho da string
-
-// Alguma função para limpar e gerir a memória
-// (free, malloc, etc.)
-// (não esquecer de libertar a stash no final)
+// USAR LINKED LISTS, PORQUE A LOGICA DE LINKED LISTS PARA ESTE PROJETO, VAI 
+// AUXILIAR NOS PROJETOS FUTUROS.
+// VAMOS TENTAR, EM VEZ DE USAR ARRAYS.
