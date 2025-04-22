8f188ffa1cc48dfcb04b62d180d5a6271c42b24a
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2025-04-22 01:01:43 +0100

    refactor get_next_line: simplify string handling and improve memory management

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tfilipe- <tfilipe-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:38:06 by tfilipe-          #+#    #+#             */
-/*   Updated: 2025/04/22 00:31:59 by tfilipe-         ###   ########.fr       */
+/*   Updated: 2025/04/22 01:01:01 by tfilipe-         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -30,44 +30,39 @@ int	has_newline(char *stash)
 }
 
 
+#include <stdlib.h>
+
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*newstring;
-	int		len1;
-	int		len2;
-	int		i;
-	int		j;
+	int		len1 = 0;
+	int		len2 = 0;
+	int		i = 0;
+	int		j = 0;
 
-	len1 = 0;
-	len2 = 0;
 	if (!s2)
 		return (NULL);
 	while (s1 && s1[len1])
 		len1++;
-	while (s2 && s2[len2])
+	while (s2[len2])
 		len2++;
-	newstring = malloc(sizeof(char) * ((len1 + len2) + 1));
+	newstring = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!newstring)
-	{
-		free(s1);
 		return (NULL);
-	}
-	i = 0;
 	while (i < len1)
 	{
 		newstring[i] = s1[i];
 		i++;
 	}
-	j = 0;
 	while (j < len2)
 	{
 		newstring[i + j] = s2[j];
 		j++;
 	}
 	newstring[i + j] = '\0';
-	free(s1);
 	return (newstring);
 }
+
 char	*extract_line(char *stash)
 {
 	char	*newline;
@@ -83,7 +78,10 @@ char	*extract_line(char *stash)
 		i++;
 	newline = malloc(sizeof(char) * (i + 1));
 	if (!newline)
+	{
+		// free(stash);	
 		return (NULL);
+	}
 	j = 0;
 	while (j < i)
 	{
@@ -135,25 +133,32 @@ char	*update_stash(char *stash)
 static char	*read_into_stash(int fd, char *stash, char *buffer)
 {
 	ssize_t	bytes_read;
+	char	*temp;
 
 	bytes_read = 1;
 	while (!has_newline(stash) && bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
-        {
-            free(stash);
-            return (NULL);
-        }
-        if (bytes_read == 0)
-            break;
-		buffer[bytes_read] = '\0';
-		stash = ft_strjoin(stash, buffer);
-		if (!stash)
+		{
+			free(stash);
 			return (NULL);
+		}
+		if (bytes_read == 0)
+			break;
+		buffer[bytes_read] = '\0';
+		temp = ft_strjoin(stash, buffer);
+		if (!temp)
+		{
+			free(stash);
+			return (NULL);
+		}
+		free(stash);
+		stash = temp;
 	}
 	return (stash);
 }
+
 
 
 char	*get_next_line(int fd)
@@ -169,12 +174,8 @@ char	*get_next_line(int fd)
 		return (NULL);
 	stash = read_into_stash(fd, stash, buffer);
 	free(buffer);
-	if (stash == NULL || *stash == '\0')
-	{
-		free(stash);
-		stash = NULL;
+	if (!stash)
 		return (NULL);
-	}
 	line = extract_line(stash);
 	stash = update_stash(stash);
 	return (line);
